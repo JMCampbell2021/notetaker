6d5cdbd7d55d515abb130c873b01f5cbc966529f
Joseph <92699311+JMCampbell2021@users.noreply.github.com>
2022-02-13 18:46:48 -0500

    intial commit

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
 const express = require("express");
 const fs = require("fs");
 const notes = require("./db/db.json");
+const uuid = require("uuid");
+const path = require("path");
 
 
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+// Middleware
+app.use(express.urlencoded({ extended: true}));
+app.use(express.json());
+app.use(express.static("public"));
